d4d07d50b89df19e9f2335f229a3b5865773c61c
Louise Skinnari <louise.skinnari@cern.ch>
2019-03-11 20:28:37 +0100

    propagate change from abhijith/andrew

--- interface/FPGAVariance.hh
+++ interface/FPGAVariance.hh
@@ -67,24 +67,24 @@ public:
       if (disk>0) {
 	double zproj=stub.z();
 	double tmp=rinv*(zproj-z0)/(2.0*t);
-	double rproj=(2.0/rinv)*sin(tmp);
+	//double rproj=(2.0/rinv)*sin(tmp);
 	double phiproj=phi0-tmp;
 	double dphi=phiproj-stub.phi();
 	if (dphi>0.5*two_pi) dphi-=two_pi;
 	if (dphi<-0.5*two_pi) dphi+=two_pi;
 	diskresidphi[disk-1]=dphi*stub.r();
-	diskresidr[disk-1]=rproj-stub.r();
+	//diskresidr[disk-1]=rproj-stub.r();
       }
 
       if (layer>0) {
 	double rproj=stub.r();
 	double phiproj=phi0-asin(0.5*rproj*rinv);
-	double zproj=z0+(2*t/rinv)*asin(0.5*rproj*rinv);
+	//double zproj=z0+(2*t/rinv)*asin(0.5*rproj*rinv);
 	double dphi=phiproj-stub.phi();
 	if (dphi>0.5*two_pi) dphi-=two_pi;
 	if (dphi<-0.5*two_pi) dphi+=two_pi;
 	layerresidphi[layer-1]=dphi*stub.r();
-	layerresidz[layer-1]=zproj-stub.z();
+	//layerresidz[layer-1]=zproj-stub.z();
       }
 
     }

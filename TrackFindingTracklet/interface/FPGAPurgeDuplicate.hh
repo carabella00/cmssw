626a082814c58368b0f48c118fbc3f93b8567f32
Anthony Lefeld <alefeld@alumni.nd.edu>
2019-04-03 15:21:04 -0400

    Added HybridFit to PurgeDuplicate as well

--- TrackFindingTracklet/interface/FPGAPurgeDuplicate.hh
+++ TrackFindingTracklet/interface/FPGAPurgeDuplicate.hh
@@ -8,6 +8,7 @@
 #include "DataFormats/L1TrackTrigger/interface/TTStub.h"
 #include "L1Trigger/TrackFindingTMTT/interface/L1track3D.h"
 #include "L1Trigger/TrackFindingTMTT/interface/KFParamsComb.h"
+#include "L1Trigger/TrackFindingTracklet/interface/FPGAHybridFit.hh"
 #endif
 
 using namespace std;
@@ -210,171 +211,184 @@ public:
         FPGATracklet* tracklet = inputtracklets_[itrk];
         std::vector<std::pair<FPGAStub*,L1TStub*>> trackstublist = inputstublists_[itrk];
 
-        std::vector<const TMTT::Stub*> TMTTstubs;
-        std::map<unsigned int, L1TStub*> L1StubIndices;
-        unsigned int L1stubID = 0;
+        FPGAHybridFit hybridFitter(iSector_);
+        hybridFitter.Fit(tracklet, trackstublist);
 
-        static TMTT::Settings* settings = new TMTT::Settings();
+        if(0){
+          std::vector<const TMTT::Stub*> TMTTstubs;
+          std::map<unsigned int, L1TStub*> L1StubIndices;
+          unsigned int L1stubID = 0;
 
-        for (unsigned int k=0;k<trackstublist.size();k++) {
-          L1TStub* L1stubptr=trackstublist[k].second;
+          static TMTT::Settings* settings = new TMTT::Settings();
 
-          double kfphi=L1stubptr->phi();
-          double kfr=L1stubptr->r();
-          double kfz=L1stubptr->z();
-          double kfbend=L1stubptr->bend();
-          bool psmodule = L1stubptr->isPSmodule();
-          unsigned int iphi = L1stubptr->iphi();
-          double alpha = L1stubptr->alpha();
+          for (unsigned int k=0;k<trackstublist.size();k++) {
+            L1TStub* L1stubptr=trackstublist[k].second;
 
-          bool isBarrel = trackstublist[k].first->isBarrel();
-          int kflayer;
+            double kfphi=L1stubptr->phi();
+            double kfr=L1stubptr->r();
+            double kfz=L1stubptr->z();
+            double kfbend=L1stubptr->bend();
+            bool psmodule = L1stubptr->isPSmodule();
+            unsigned int iphi = L1stubptr->iphi();
+            double alpha = L1stubptr->alpha();
 
-          // Barrel-specific
-          if (isBarrel) {
-            kflayer=L1stubptr->layer()+1;
-            if (printDebugKF) cout << "Will create layer stub with : ";
+            bool isBarrel = trackstublist[k].first->isBarrel();
+            int kflayer;
 
-          // Disk-specific
-          } else {
-            kflayer=abs(L1stubptr->disk());
-            if (kfz>0) {
-              kflayer+=10;
+            // Barrel-specific
+            if (isBarrel) {
+              kflayer=L1stubptr->layer()+1;
+              if (printDebugKF) cout << "Will create layer stub with : ";
+
+            // Disk-specific
             } else {
-              kflayer+=20;
+              kflayer=abs(L1stubptr->disk());
+              if (kfz>0) {
+                kflayer+=10;
+              } else {
+                kflayer+=20;
+              }
+              if (printDebugKF) cout << "Will create disk stub with : ";
             }
-            if (printDebugKF) cout << "Will create disk stub with : ";
+
+           /* edm::ESHandle<TrackerGeometry> trackerGeometryHandle;
+	      iSetup.get<TrackerDigiGeometryRecord>().get( trackerGeometryHandle );
+
+	      const TrackerGeometry*  trackerGeometry = trackerGeometryHandle.product();
+      */
+      /*	edm::ESHandle<TrackerTopology> trackerTopologyHandle;
+	      iSetup.get<TrackerTopologyRcd>().get(trackerTopologyHandle);/
+	      const TrackerTopology*  trackerTopology = trackerTopologyHandle.product();
+      */	
+
+
+            if (printDebugKF) cout <<kfphi<<" "<<kfr<<" "<<kfz<<" "<<kfbend<<" "<<kflayer<<" "<<isBarrel<<" "<<psmodule<<" "<<endl;
+            TMTT::Stub* TMTTstubptr= new TMTT::Stub(kfphi, kfr, kfz, kfbend, kflayer, psmodule, isBarrel, iphi, -alpha, settings, nullptr, L1stubID);
+            TMTTstubs.push_back(TMTTstubptr);
+            L1StubIndices[L1stubID++] = L1stubptr;
           }
 
-         /* edm::ESHandle<TrackerGeometry> trackerGeometryHandle;
-	    iSetup.get<TrackerDigiGeometryRecord>().get( trackerGeometryHandle );
-
-	    const TrackerGeometry*  trackerGeometry = trackerGeometryHandle.product();
-    */
-    /*	edm::ESHandle<TrackerTopology> trackerTopologyHandle;
-	    iSetup.get<TrackerTopologyRcd>().get(trackerTopologyHandle);/
-	    const TrackerTopology*  trackerTopology = trackerTopologyHandle.product();
-    */	
+          if (printDebugKF) cout << "Made stubs: trackstublist.size() = " << trackstublist.size()<< endl;
 
 
-          if (printDebugKF) cout <<kfphi<<" "<<kfr<<" "<<kfz<<" "<<kfbend<<" "<<kflayer<<" "<<isBarrel<<" "<<psmodule<<" "<<endl;
-          TMTT::Stub* TMTTstubptr= new TMTT::Stub(kfphi, kfr, kfz, kfbend, kflayer, psmodule, isBarrel, iphi, -alpha, settings, nullptr, L1stubID);
-          TMTTstubs.push_back(TMTTstubptr);
-          L1StubIndices[L1stubID++] = L1stubptr;
-        }
+          double kfrinv=tracklet->rinvapprox();
+          double kfphi0=tracklet->phi0approx();
+          double kfz0=tracklet->z0approx();
+          double kft=tracklet->tapprox();
 
-        if (printDebugKF) cout << "Made stubs: trackstublist.size() = " << trackstublist.size()<< endl;
-
-
-        double kfrinv=tracklet->rinvapprox();
-        double kfphi0=tracklet->phi0approx();
-        double kfz0=tracklet->z0approx();
-        double kft=tracklet->tapprox();
-
-        if (printDebugKF) {
-         std::cout << "tracklet phi0 = "<< kfphi0 << std::endl;
-         std::cout << "iSector = " << iSector_ << std::endl;
-         std::cout << "dphisectorHG = " << dphisectorHG << std::endl;
-        }
-
-        // IRT bug fix
-        //kfphi0 = kfphi0 + iSector_*2*M_PI/NSector - 0.5*dphisectorHG - M_PI;
-        kfphi0 = kfphi0 + iSector_*2*M_PI/NSector - 0.5*dphisectorHG;
-
-        if (kfphi0>M_PI) kfphi0-=2*M_PI;
-        if (kfphi0<-M_PI) kfphi0+=2*M_PI;
-
-        std::pair<unsigned int, unsigned int> celllocation(1,1);
-        //    std::pair<float,float> helixrphi(300*kfrinv/settings->getBfield(),kfphi0);
-        std::pair<float,float> helixrphi(kfrinv*1.0e11/(2.9979e8*settings->getBfield()),kfphi0);
-        std::pair<float,float> helixrz(kfz0,kft);
-
-        //  TMTT phi sector definition: phiCentre_ = 2.*M_PI * (0.5 + float(iPhiSec)) / float(settings->numPhiSectors()) - M_PI; // Centre of sector in phi
-
-        unsigned int kf_eta_reg;
-
-        /*   
-    72:  float   tanLambda()  const  {return helixRz_.second;}
-    73:  float   theta()      const  {return atan2(1., this->tanLambda());} // Use atan2 to ensure 0 < theta < pi.
-    78:  float   zAtChosenR()   const  {return (this->z0() + (settings_->chosenRofZ()) * this->tanLambda());} // neglects transverse impact parameter & track curvature.
-    */
-        float  kfz50=kfz0+50.0*kft;
-
-        if (kfz50 > 214.0){kf_eta_reg=17;}
-        else if (kfz50 > 172.0){kf_eta_reg=16;}
-        else if (kfz50 > 132.0){kf_eta_reg=15;}
-        else if (kfz50 > 98.0){kf_eta_reg=14;}
-        else if (kfz50 > 72.0){kf_eta_reg=13;}
-        else if (kfz50 > 51.0){kf_eta_reg=12;}
-        else if (kfz50 > 32.0){kf_eta_reg=11;}
-        else if (kfz50 > 16.0){kf_eta_reg=10;}
-        else if (kfz50 > 0.0){kf_eta_reg=9;}
-        else if (kfz50 > -16.0){kf_eta_reg=8;}
-        else if (kfz50 > -32.0){kf_eta_reg=7;}
-        else if (kfz50 > -51.0){kf_eta_reg=6;}
-        else if (kfz50 > -72.0){kf_eta_reg=5;}
-        else if (kfz50 > -98.0){kf_eta_reg=4;}
-        else if (kfz50 > -132.0){kf_eta_reg=3;}
-        else if (kfz50 > -172.0){kf_eta_reg=2;}
-        else if (kfz50 > -214.0){kf_eta_reg=1;}
-        else {kf_eta_reg=0;}
-
-        int kf_phi_sec=tracklet->homeSector() -3;
-        if(kf_phi_sec < 0){kf_phi_sec+=9;}      
-
-
-        TMTT::L1track3D l1track3d(settings,TMTTstubs,celllocation,helixrphi,helixrz,kf_phi_sec,kf_eta_reg,1,false);
-
-        // Create Kalman track fitter.
-        static bool firstPrint = true;
-    #ifdef USE_HLS
-        if (firstPrint) cout << "Will make KFParamsCombHLS for " << nHelixPar << " param fit" << endl;
-        static TMTT::TrackFitGeneric* fitterKF = new TMTT::KFParamsCombCallHLS(settings, nHelixPar, "KFfitterHLS");
-    #else
-        if (firstPrint) cout << "Will make KFParamsComb for " << nHelixPar << " param fit"<< endl;
-        static TMTT::TrackFitGeneric* fitterKF = new TMTT::KFParamsComb(settings, nHelixPar, "KFfitter");
-    #endif
-        firstPrint = false;
-
-        //  cout << "Will call fit" << endl;
-        //fitterKF->fit(l1track3d,1,kf_eta_reg);
-
-        TMTT::L1fittedTrack fittedTrk = fitterKF->fit(l1track3d); 
-       
-        TMTT::KFTrackletTrack trk = fittedTrk.returnKFTrackletTrack();
-
-        if (printDebugKF) cout << "Done with Kalman fit. Pars: pt = " << trk.pt() << ", 1/2R = " << 3.8*3*trk.qOverPt()/2000 << ", phi0 = " << trk.phi0() << ", eta = " << trk.eta() << ", z0 = " << trk.z0() << ", chi2 = "<<trk.chi2()  << ", accepted = "<< trk.accepted() << endl;
-
-        // IRT bug fix
-        //double tracklet_phi0=M_PI+trk.phi0()-iSector_*2*M_PI/NSector+0.5*dphisectorHG;
-        double tracklet_phi0=trk.phi0()-iSector_*2*M_PI/NSector+0.5*dphisectorHG;
-
-        if (tracklet_phi0>M_PI) tracklet_phi0-=2*M_PI;
-        if (tracklet_phi0<-M_PI) tracklet_phi0+=2*M_PI;
-
-        double rinvfit=0.01*0.3*settings->getBfield()*trk.qOverPt();
-
-        int id0fit   = trk.d0()  / kd0;
-
-        if(trk.accepted()){
-
-          const vector<const TMTT::Stub*>& stubsFromFit = trk.getStubs();
-          vector<L1TStub*> l1stubsFromFit;
-          for (const TMTT::Stub* s : stubsFromFit) {
-              unsigned int IDf = s->index();
-              L1TStub* l1s = L1StubIndices.at(IDf);
-              l1stubsFromFit.push_back(l1s);
+          if (printDebugKF) {
+           std::cout << "tracklet phi0 = "<< kfphi0 << std::endl;
+           std::cout << "iSector = " << iSector_ << std::endl;
+           std::cout << "dphisectorHG = " << dphisectorHG << std::endl;
           }
 
-          if (printDebugKF) cout<<"#stubs before/after KF fit = "<<TMTTstubs.size()<<"/"<<l1stubsFromFit.size()<<endl;
+          // IRT bug fix
+          //kfphi0 = kfphi0 + iSector_*2*M_PI/NSector - 0.5*dphisectorHG - M_PI;
+          kfphi0 = kfphi0 + iSector_*2*M_PI/NSector - 0.5*dphisectorHG;
 
-         tracklet->setFitPars(rinvfit,tracklet_phi0,trk.d0(),sinh(trk.eta()),trk.z0(),
-           trk.chi2(),rinvfit,tracklet_phi0, trk.d0(), sinh(trk.eta()),
-           trk.z0(),trk.chi2(),rinvfit/krinvpars,
-           tracklet_phi0/kphi0pars,id0fit,
-           sinh(trk.eta())/ktpars,trk.z0()/kz0pars,trk.chi2(),l1stubsFromFit);
-         //cout<<" KF fit d0 is "<<trk.d0()<<"\n";
+          if (kfphi0>M_PI) kfphi0-=2*M_PI;
+          if (kfphi0<-M_PI) kfphi0+=2*M_PI;
 
+          std::pair<unsigned int, unsigned int> celllocation(1,1);
+          //    std::pair<float,float> helixrphi(300*kfrinv/settings->getBfield(),kfphi0);
+          std::pair<float,float> helixrphi(kfrinv*1.0e11/(2.9979e8*settings->getBfield()),kfphi0);
+          std::pair<float,float> helixrz(kfz0,kft);
+
+          //  TMTT phi sector definition: phiCentre_ = 2.*M_PI * (0.5 + float(iPhiSec)) / float(settings->numPhiSectors()) - M_PI; // Centre of sector in phi
+
+          unsigned int kf_eta_reg;
+
+          /*   
+      72:  float   tanLambda()  const  {return helixRz_.second;}
+      73:  float   theta()      const  {return atan2(1., this->tanLambda());} // Use atan2 to ensure 0 < theta < pi.
+      78:  float   zAtChosenR()   const  {return (this->z0() + (settings_->chosenRofZ()) * this->tanLambda());} // neglects transverse impact parameter & track curvature.
+      */
+          float  kfz50=kfz0+50.0*kft;
+
+          if (kfz50 > 214.0){kf_eta_reg=17;}
+          else if (kfz50 > 172.0){kf_eta_reg=16;}
+          else if (kfz50 > 132.0){kf_eta_reg=15;}
+          else if (kfz50 > 98.0){kf_eta_reg=14;}
+          else if (kfz50 > 72.0){kf_eta_reg=13;}
+          else if (kfz50 > 51.0){kf_eta_reg=12;}
+          else if (kfz50 > 32.0){kf_eta_reg=11;}
+          else if (kfz50 > 16.0){kf_eta_reg=10;}
+          else if (kfz50 > 0.0){kf_eta_reg=9;}
+          else if (kfz50 > -16.0){kf_eta_reg=8;}
+          else if (kfz50 > -32.0){kf_eta_reg=7;}
+          else if (kfz50 > -51.0){kf_eta_reg=6;}
+          else if (kfz50 > -72.0){kf_eta_reg=5;}
+          else if (kfz50 > -98.0){kf_eta_reg=4;}
+          else if (kfz50 > -132.0){kf_eta_reg=3;}
+          else if (kfz50 > -172.0){kf_eta_reg=2;}
+          else if (kfz50 > -214.0){kf_eta_reg=1;}
+          else {kf_eta_reg=0;}
+
+          int kf_phi_sec=tracklet->homeSector() -3;
+          if(kf_phi_sec < 0){kf_phi_sec+=9;}      
+
+
+          TMTT::L1track3D l1track3d(settings,TMTTstubs,celllocation,helixrphi,helixrz,kf_phi_sec,kf_eta_reg,1,false);
+
+          // Create Kalman track fitter.
+          static bool firstPrint = true;
+      #ifdef USE_HLS
+          if (firstPrint) cout << "Will make KFParamsCombHLS for " << nHelixPar << " param fit" << endl;
+          static TMTT::TrackFitGeneric* fitterKF = new TMTT::KFParamsCombCallHLS(settings, nHelixPar, "KFfitterHLS");
+      #else
+          if (firstPrint) cout << "Will make KFParamsComb for " << nHelixPar << " param fit"<< endl;
+          static TMTT::TrackFitGeneric* fitterKF = new TMTT::KFParamsComb(settings, nHelixPar, "KFfitter");
+      #endif
+          firstPrint = false;
+
+          //  cout << "Will call fit" << endl;
+          //fitterKF->fit(l1track3d,1,kf_eta_reg);
+
+          TMTT::L1fittedTrack fittedTrk = fitterKF->fit(l1track3d); 
+         
+          TMTT::KFTrackletTrack trk = fittedTrk.returnKFTrackletTrack();
+
+          if (printDebugKF) cout << "Done with Kalman fit. Pars: pt = " << trk.pt() << ", 1/2R = " << 3.8*3*trk.qOverPt()/2000 << ", phi0 = " << trk.phi0() << ", eta = " << trk.eta() << ", z0 = " << trk.z0() << ", chi2 = "<<trk.chi2()  << ", accepted = "<< trk.accepted() << endl;
+
+          // IRT bug fix
+          //double tracklet_phi0=M_PI+trk.phi0()-iSector_*2*M_PI/NSector+0.5*dphisectorHG;
+          double tracklet_phi0=trk.phi0()-iSector_*2*M_PI/NSector+0.5*dphisectorHG;
+
+          if (tracklet_phi0>M_PI) tracklet_phi0-=2*M_PI;
+          if (tracklet_phi0<-M_PI) tracklet_phi0+=2*M_PI;
+
+          double rinvfit=0.01*0.3*settings->getBfield()*trk.qOverPt();
+
+          int id0fit   = trk.d0()  / kd0;
+
+          if(trk.accepted()){
+
+            const vector<const TMTT::Stub*>& stubsFromFit = trk.getStubs();
+            vector<L1TStub*> l1stubsFromFit;
+            for (const TMTT::Stub* s : stubsFromFit) {
+                unsigned int IDf = s->index();
+                L1TStub* l1s = L1StubIndices.at(IDf);
+                l1stubsFromFit.push_back(l1s);
+            }
+
+            if (printDebugKF) cout<<"#stubs before/after KF fit = "<<TMTTstubs.size()<<"/"<<l1stubsFromFit.size()<<endl;
+
+          tracklet->setFitPars(rinvfit,tracklet_phi0,trk.d0(),sinh(trk.eta()),trk.z0(),
+            trk.chi2(),rinvfit,tracklet_phi0, trk.d0(), sinh(trk.eta()),
+            trk.z0(),trk.chi2(),rinvfit/krinvpars,
+            tracklet_phi0/kphi0pars,id0fit,
+            sinh(trk.eta())/ktpars,trk.z0()/kz0pars,trk.chi2(),l1stubsFromFit);
+          //cout<<" KF fit d0 is "<<trk.d0()<<"\n";
+
+
+          } else {
+            if (printDebugKF) cout << "FPGAFitTrack:KF rejected track"<<endl;
+          }
+
+        } // end if(0)
+
+        // If the track was accepted (and thus fit), add to output
+        if(tracklet.fit()) {
           // Add track to output if it wasn't merged into another
           FPGATrack* outtrack = tracklet->getTrack();
           outtrack->setSector(iSector_);
@@ -385,12 +399,7 @@ public:
           outtrack->setStubIDpremerge(inputstubidslists_[itrk]);
           outtrack->setStubIDprefit(mergedstubidslists_[itrk]);
           outputtracks_.push_back(outtrack);
-
-        } else {
-          if (printDebugKF) cout << "FPGAFitTrack:KF rejected track"<<endl;
         }
-
-
       }
     }
     #endif
